7dad5f6e174e8c12ae92c19d680cedb4a64394eb
wayusei <r.roszam@gmail.com>
2022-07-21 20:28:19 -0500

    ejemplo/ejercicios if saludo

--- S2.js
+++ S2.js
@@ -1,8 +1,17 @@
 //Ejercicio 1 sesion 2
 
-const time = 13;
+const time = 11;
 let greeting;
 
-if(time < 12){
+if (time >= 24 || time < 0){
+    greeting = 'time debe tener valores entre 0 y 23'
+} else if (time >= 0 && time < 12){
     greeting = 'Buenos días!'
+} else if (time < 19){
+    greeting = 'Buenas tardes!'
+} else if (time >= 19){
+    greeting = 'Buenas noches!'
 }
+
+console.log(greeting);
+alert(greeting);
