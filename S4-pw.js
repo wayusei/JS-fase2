acf46d30174b8a917277012714fa88e05f26793c
Rodrigo Rosas Zamudio <rrosas@ucc.mx>
2022-07-31 23:54:24 -0500

    fin de postwork sesion 4

--- S4-pw.js
+++ S4-pw.js
@@ -12,16 +12,35 @@
 // La función Object.keys() es útil para obtener las propiedades de los objetos.
 
 function deepEqual(a, b) {
-    if ((typeof(a) = typeof(b)) || (a === b)) {
+    if (typeof(a) === 'object' && typeof(b) === 'object'){
+        const keysA = Object.keys(a).sort();
+        const keysB = Object.keys(b).sort();
+    
+        if (!deepEqual(keysA.length, keysB.length)){
+            return false;
+        }
+
+        for(let i = 0; i < keysA.length; i++){
+            if(!deepEqual(keysA[i], keysB[i])){
+                return false;
+            }
+            if(!deepEqual(a[keysA[i]], b[keysB[i]])){
+                return false;
+            }
+        }
         return true;
-    } 
+    }else if (a === b){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 const john = {
     firstName: 'John',
     lastName: 'Doe'
 }
-
+console.log('Deep Equal');
 console.log('Test 1:', deepEqual(1, 1)) // true
 console.log('Test 2:', deepEqual(1, '1')) // false
 console.log('Test 3:', deepEqual(john, john)) // true
@@ -33,11 +52,21 @@ console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
 // en múltiples arreglos del tamaño determinado por size.
 
 function chunk(array, size) {
-    // Code goes here
-};
+    const resultado = [];
+    const recorridos = array.length / size;
+    let inicio = 0;
+    let fin = size;
+    for (let i = 0; i < recorridos; i++){
+        resultado.push(array.slice(inicio, fin));
+        inicio = fin;
+        fin += size;
+    }
+    return resultado;
+}
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8]
 
+console.log('Chunk');
 console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
 console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
 console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
@@ -51,9 +80,24 @@ console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
 
 
 function frequency(string) {
-    // Code goes here
+    const resultado = {}; 
+    for (caracter of string){
+        let frecuencia = 0;
+        for (let i = 0; i < string.length; i++){
+            if (string[i] === caracter){
+                frecuencia++; 
+            }
+        }
+        resultado[caracter] = frecuencia;            
+    }
+    let clavesResultado = Object.keys(resultado).sort();
+    let ordenado = {};
+    clavesResultado.forEach(elemento => ordenado[elemento] = resultado[elemento]);
+    
+    return ordenado;
 }
 
+console.log('Frequency');
 console.log('Test 1:', frequency('cccbbbaaa'))
 // {a: 3, b: 3, c: 3}
 console.log('Test 2:', frequency('www.bedu.org'))
